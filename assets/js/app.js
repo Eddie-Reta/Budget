85f5ef93dcae8202dbd4266709fb7a991021a395
eduardo reta rj <https://github.com/Eddie-Reta>
2020-03-01 15:21:04 -0500

    data is passed into the console

---
+++ assets/js/app.js
@@ -0,0 +1,69 @@
+
+//Budeget Controller
+
+var budgetController = (function(){
+    
+    
+
+})();
+
+// UI Controller
+
+var UIController = (function(){
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    return {
+        getInput: function() {
+            return{
+            //Will grab the income or the expense..
+            type: document.querySelector(DOMstrings.inputType).value,
+
+            description: document.querySelector(DOMstrings.inputDescription).value,
+
+            value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }
+
+})();
+
+//Global App Controller
+
+var controller = (function(budgetCtrl, UICtrl){
+
+    var DOM = UICtrl.getDOMstrings();
+    
+    var ctrlAddItem = function(){
+              
+            //1. get the field input data
+            
+            var input = UICtrl.getInput();
+            console.log(input);
+
+            //2. add the item to the budget controller
+
+            
+            //3. add the item to the UI
+        //4. calculate the budget
+        //5. display the budget on the UI
+
+    };
+    //code 
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener("keypress", function(event){
+      
+        if(event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+    };
+   });
+
+})(budgetController, UIController);
