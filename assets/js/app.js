ac9205eedf4f281128d873de8315299649b57eab
eduardo reta rj <https://github.com/Eddie-Reta>
2020-03-02 22:29:54 -0500

    working on adding income and expenses to the UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -78,6 +78,27 @@ var UIController = (function(){
             value: document.querySelector(DOMstrings.inputValue).value
             };
         },
+        addListItem: function(obj, type) {
+            var html, newHtml;
+            // Create html string with placeholder text 
+            
+            if (type === "inc"){
+            html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
+            } else if  (type === 'exp'){
+            html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+            
+            // Replace the placeholder text with some actual data    
+            
+            newHtml = html.replace('%id%',obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            
+            // Insert the html into the DOM
+            
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -115,6 +136,8 @@ var controller = (function(budgetCtrl, UICtrl){
             newItem = budgetCtrl.addItem(input.type, input.description, input.value);
             
             //3. add the item to the UI
+
+
         //4. calculate the budget
         //5. display the budget on the UI
 
