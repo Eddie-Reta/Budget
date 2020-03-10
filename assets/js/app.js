b26c5d809cbf19cb5871c246f19bed257be33193
eduardo reta rj <https://github.com/Eddie-Reta>
2020-03-09 21:24:25 -0400

    passed data to update ui

--- assets/js/app.js
+++ assets/js/app.js
@@ -106,7 +106,11 @@ var UIController = (function() {
     inputValue: ".add__value",
     inputBtn: ".add__btn",
     incomeContainer: ".income__list",
-    expensesContainer: ".expenses__list"
+    expensesContainer: ".expenses__list",
+    budgetLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expensesLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage"
   };
   return {
     getInput: function() {
@@ -154,7 +158,18 @@ var UIController = (function() {
         });
         fieldsArr[0].focus();
     },
-
+  
+    displayBudget: function(obj){
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+      
+      if(obj.percentage > 0 ){
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      };
+    },
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -189,7 +204,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     
     //3. display the budget on the UI
 
-    console.log(budget);
+    UICtrl.displayBudget(budget);
 
   };
 
@@ -217,7 +232,15 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   return {
     init: function() {
+      console.log("Application has started");
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
+
     }
   };
 })(budgetController, UIController);
