896b718aa3357c60c4e9591b84883ed4f7a7719c
eduardo reta rj <https://github.com/Eddie-Reta>
2020-03-30 23:36:17 -0400

    completed app just need to work on readme

--- assets/js/app.js
+++ assets/js/app.js
@@ -176,7 +176,13 @@ var UIController = (function() {
     dec = numSplit[1];
 
     return (type === 'exp' ? sign = '-' : sign = '+') + ' ' + int + '.' + dec;
-  }
+  };
+
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    };
+  };
 
   return {
     getInput: function() {
@@ -251,12 +257,6 @@ var UIController = (function() {
 
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-      var nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        };
-      };
-
       nodeListForEach(fields, function(current, index) {
       
         if(percentages[index] > 0 ) {
@@ -281,6 +281,21 @@ var UIController = (function() {
       document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
 
     },
+
+    changedType: function() {
+
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType + ',' +
+        DOMstrings.inputDescription + ','+
+        DOMstrings.inputValue);
+
+        nodeListForEach(fields, function(cur) {
+          cur.classList.toggle('red-focus');
+        });
+
+        document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -303,6 +318,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
 
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
   };
 
   var updateBudget = function(){
